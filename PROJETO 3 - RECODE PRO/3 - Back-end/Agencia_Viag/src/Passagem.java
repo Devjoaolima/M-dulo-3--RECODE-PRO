1f1c654f03c2026f7138c51f33aab72c8f84c3bb
João Vitor Lima <98293283+Devjoaolima@users.noreply.github.com>
2023-10-14 23:16:10 -0300

    Add files via upload

---
+++ PROJETO 3 - RECODE PRO/3 - Back-end/Agencia_Viag/src/Passagem.java
@@ -0,0 +1,39 @@
+public class Passagem {
+
+    private int id;
+    private float preco;
+    private float taxas;
+    private String destino;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public float getTaxas() {
+        return taxas;
+    }
+
+    public void setTaxas(float taxas) {
+        this.taxas = taxas;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+}
---
+++ PROJETO 3 - RECODE PRO/3 - Back-end/Agencia_Viag/src/ViagemDAO.java
@@ -0,0 +1,96 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ViagemDAO {
+    public boolean EmitirViagem(Viagem passagem) {
+        String sql = "INSERT INTO Passagem (Viagemid, Usuarioid) VALUES (?, ?)";
+
+        try (Connection conexao = ConexaoMySQL.conectar();
+             PreparedStatement preparedStatement = conexao.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setInt(1, passagem.getViagemId());
+            preparedStatement.setInt(2, passagem.getUsuarioId());
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if (affectedRows == 0) {
+                System.err.println("Pedido de passagem não foi emitido.");
+            } else {
+                System.out.println("Viagem emitida com sucesso.");
+                return true; // Return true if the request is successful.
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao emitir a viagem: " + e.getMessage());
+        }
+        return false; // Return false in case of an error.
+    }
+
+    public boolean ExcluirViagem(int passagemId) {
+        String sql = "DELETE FROM Passagem WHERE Id = ?";
+
+        try (Connection conexao = ConexaoMySQL.conectar();
+             PreparedStatement preparedStatement = conexao.prepareStatement(sql)) {
+            preparedStatement.setInt(1, passagemId);
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if (affectedRows > 0) {
+                System.out.println("Viagem com ID " + passagemId + " foi excluída com sucesso.");
+                return true; // Return true if the deletion is successful.
+            } else {
+                System.err.println("Nenhuma viagem encontrada com o ID " + passagemId);
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao excluir a viagem: " + e.getMessage());
+        }
+        return false; // Return false in case of an error.
+    }
+
+    public boolean AtualizarViagem(Viagem passagem) {
+        String sql = "UPDATE Passagem SET Viagemid = ?, Usuarioid = ? WHERE Id = ?";
+
+        try (Connection conexao = ConexaoMySQL.conectar();
+             PreparedStatement preparedStatement = conexao.prepareStatement(sql)) {
+            preparedStatement.setInt(1, passagem.getViagemId());
+            preparedStatement.setInt(2, passagem.getUsuarioId());
+            preparedStatement.setInt(3, passagem.getId());
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if (affectedRows > 0) {
+                System.out.println("Viagem com ID " + passagem.getId() + " atualizada com sucesso.");
+                return true; // Return true if the update is successful.
+            } else {
+                System.err.println("Nenhuma viagem encontrada com o ID " + passagem.getId());
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao atualizar a viagem: " + e.getMessage());
+        }
+        return false; // Return false in case of an error.
+    }
+
+    public Viagem VisualizarViagem(int passagemId) {
+        String sql = "SELECT Id, Viagemid, Usuarioid FROM Passagem WHERE Id = ?";
+
+        try (Connection conexao = ConexaoMySQL.conectar();
+             PreparedStatement preparedStatement = conexao.prepareStatement(sql)) {
+            preparedStatement.setInt(1, passagemId);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Viagem passagem = new Viagem();
+                passagem.setId(resultSet.getInt("Id"));
+                passagem.setViagemId(resultSet.getInt("Viagemid"));
+                passagem.setUsuarioId(resultSet.getInt("Usuarioid"));
+                return passagem; // Return the Viagem object if the visualization is successful.
+            } else {
+                System.err.println("Nenhuma viagem encontrada com o ID " + passagemId);
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao visualizar a viagem: " + e.getMessage());
+        }
+        return null; // Return null in case of an error or when no data is found.
+    }
+}
